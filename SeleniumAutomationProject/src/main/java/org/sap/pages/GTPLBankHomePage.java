1ca7cabab6cc7037b4e0a80148565d13cbb0ade1
devi <tirumala_kadiyam@grhombus.com>
2023-09-02 15:19:26 +0530

    customerPage

--- SeleniumAutomationProject/src/main/java/org/sap/pages/GTPLBankHomePage.java
+++ SeleniumAutomationProject/src/main/java/org/sap/pages/GTPLBankHomePage.java
@@ -6,10 +6,20 @@ import org.sap.enums.WaitStrategy;
 public class GTPLBankHomePage extends BasePage {
 
 	 private final By linkLogout=By.linkText("Log out");
+	 private final By NewCustomerLinkBy = By.linkText("New Customer");
 	 
-	 public void clickButton() {
+	 
+		 
+	 public GTPLBankLoginPage clickLogoutLink() {
 		 click(linkLogout, WaitStrategy.PRESENCE, "Logoutlink");
+		 return new GTPLBankLoginPage();
 	 }
+	 
+	 public GTPLNewCustomerPage clickNewCustomerLink() {
+		 click(NewCustomerLinkBy, WaitStrategy.PRESENCE, "NewCustomerlink");
+		 return  new GTPLNewCustomerPage();
+	 }
+	 
 		public String getTitle() {
 			return getPageTitle();
 
